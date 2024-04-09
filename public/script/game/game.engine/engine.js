fdba9d1ed7fb810dd42a1837ff6420b55d82bb8b
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-04-10 01:38:09 +0530

    feat: Add most famou game of life patterns to the game

--- public/script/game/game.engine/engine.js
+++ public/script/game/game.engine/engine.js
@@ -15,7 +15,7 @@ export default class GameEngine {
     this.deadCellColor = "black";
 
     this.handleEvents();
-    this.initialize();
+    // this.initialize();
     this.initializePlot();
 
     this.prevHightlightedArea = null;
